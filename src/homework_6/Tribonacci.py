9c914226ceb2ca397b63f96ed030e36ebffd09bc
kuleshovaleksandr <drevenb@gmail.com>
2025-04-21 21:20:32 +0300

    Tribonacci.py modified

--- src/homework_6/Tribonacci.py
+++ src/homework_6/Tribonacci.py
@@ -1,9 +1,7 @@
 class Tribonacci:
-
-    nums = []
-
     def __init__(self, size):
         self.index = 0
+        self.a, self.b, self.c = 1, 1, 1
         self.size = size
 
     def __next__(self):
@@ -11,11 +9,11 @@ class Tribonacci:
         if self.index > self.size:
             raise StopIteration
         if self.index == 1 or self.index == 2 or self.index == 3:
-            self.nums.append(1)
+            return 1
         if self.index >= 4:
-            num = self.nums[self.index - 2] + self.nums[self.index - 3] + self.nums[self.index - 4]
-            self.nums.append(num)
-        return self.nums[self.index - 1]
+            result = self.a + self.b + self.c
+            self.a, self.b, self.c = self.b, self.c, result
+            return result
 
     def __iter__(self):
         return self
